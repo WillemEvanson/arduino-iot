7dc87b499116e3bf80dee8b5a08d82c26d3e819b
David Amebley <davameb@gmail.com>
2025-12-03 11:00:29 -0600

    Refactor code formatting and update MQTT server address

--- sketch_dec1a/sketch_dec1a.ino
+++ sketch_dec1a/sketch_dec1a.ino
@@ -11,17 +11,17 @@
 #include <string.h>
 
 const uint8_t HMAC_KEY[32] = {
-  0x71, 0x33, 0x54, 0x02, 0x77, 0xE6, 0x27, 0x8E,
-  0x0F, 0x52, 0xC3, 0x91, 0x5A, 0x8A, 0xAF, 0x74,
-  0xAB, 0x56, 0xCE, 0xF7, 0xED, 0x2E, 0x50, 0x91,
-  0xEC, 0x36, 0x6B, 0xE2, 0xB2, 0xF0, 0x71, 0xDC
-};
+    0x71, 0x33, 0x54, 0x02, 0x77, 0xE6, 0x27, 0x8E,
+    0x0F, 0x52, 0xC3, 0x91, 0x5A, 0x8A, 0xAF, 0x74,
+    0xAB, 0x56, 0xCE, 0xF7, 0xED, 0x2E, 0x50, 0x91,
+    0xEC, 0x36, 0x6B, 0xE2, 0xB2, 0xF0, 0x71, 0xDC};
 
 const char *WIFI_SSID = "laptop";
 const char *WIFI_PASSWORD = "password";
 
 const char *MQTT_CLIENT_NAME = "ESP8266Client";
-const char *MQTT_SERVER_NAME = "10.42.0.1";
+// const char *MQTT_SERVER_NAME = "10.42.0.1";  // original
+const char *MQTT_SERVER_NAME = "10.147.144.34";
 const int MQTT_PORT = 1883;
 
 WiFiUDP ntpUDP;
@@ -35,23 +35,25 @@ size_t encodeTemperature(uint8_t *buf, size_t buf_size,
                          uint64_t ts,
                          double temperature)
 {
-    CborEncoder encoder, array;
-    cbor_encoder_init(&encoder, buf, buf_size, 0);
+  CborEncoder encoder, array;
+  cbor_encoder_init(&encoder, buf, buf_size, 0);
 
-    CborError err = cbor_encoder_create_array(&encoder, &array, 5);
-    if (err != CborNoError) return 0;
+  CborError err = cbor_encoder_create_array(&encoder, &array, 5);
+  if (err != CborNoError)
+    return 0;
 
-    cbor_encode_uint(&array, (uint64_t) 1);
-    cbor_encode_text_stringz(&array, device_id);
-    cbor_encode_uint(&array, ts);
-    cbor_encode_double(&array, temperature);
-    uint8_t sig[32] = {0};
-    cbor_encode_byte_string(&array, sig, sizeof(sig));
+  cbor_encode_uint(&array, (uint64_t)1);
+  cbor_encode_text_stringz(&array, device_id);
+  cbor_encode_uint(&array, ts);
+  cbor_encode_double(&array, temperature);
+  uint8_t sig[32] = {0};
+  cbor_encode_byte_string(&array, sig, sizeof(sig));
 
-    err = cbor_encoder_close_container(&encoder, &array);
-    if (err != CborNoError) return 0;
+  err = cbor_encoder_close_container(&encoder, &array);
+  if (err != CborNoError)
+    return 0;
 
-    return cbor_encoder_get_buffer_size(&encoder, buf);
+  return cbor_encoder_get_buffer_size(&encoder, buf);
 }
 
 size_t encodeMotion(uint8_t *buf, size_t buf_size,
@@ -59,23 +61,25 @@ size_t encodeMotion(uint8_t *buf, size_t buf_size,
                     uint64_t ts,
                     bool motion)
 {
-    CborEncoder encoder, array;
-    cbor_encoder_init(&encoder, buf, buf_size, 0);
+  CborEncoder encoder, array;
+  cbor_encoder_init(&encoder, buf, buf_size, 0);
 
-    CborError err = cbor_encoder_create_array(&encoder, &array, 5);
-    if (err != CborNoError) return 0;
+  CborError err = cbor_encoder_create_array(&encoder, &array, 5);
+  if (err != CborNoError)
+    return 0;
 
-    cbor_encode_uint(&array, (uint64_t) 2);
-    cbor_encode_text_stringz(&array, device_id);
-    cbor_encode_uint(&array, ts);
-    cbor_encode_boolean(&array, motion);
-    uint8_t sig[32] = {0};
-    cbor_encode_byte_string(&array, sig, sizeof(sig));
+  cbor_encode_uint(&array, (uint64_t)2);
+  cbor_encode_text_stringz(&array, device_id);
+  cbor_encode_uint(&array, ts);
+  cbor_encode_boolean(&array, motion);
+  uint8_t sig[32] = {0};
+  cbor_encode_byte_string(&array, sig, sizeof(sig));
 
-    err = cbor_encoder_close_container(&encoder, &array);
-    if (err != CborNoError) return 0;
+  err = cbor_encoder_close_container(&encoder, &array);
+  if (err != CborNoError)
+    return 0;
 
-    return cbor_encoder_get_buffer_size(&encoder, buf);
+  return cbor_encoder_get_buffer_size(&encoder, buf);
 }
 
 size_t encodeDoor(uint8_t *buf, size_t buf_size,
@@ -83,47 +87,51 @@ size_t encodeDoor(uint8_t *buf, size_t buf_size,
                   uint64_t ts,
                   bool door_open)
 {
-    CborEncoder encoder, array;
-    cbor_encoder_init(&encoder, buf, buf_size, 0);
+  CborEncoder encoder, array;
+  cbor_encoder_init(&encoder, buf, buf_size, 0);
 
-    CborError err = cbor_encoder_create_array(&encoder, &array, 5);
-    if (err != CborNoError) return 0;
+  CborError err = cbor_encoder_create_array(&encoder, &array, 5);
+  if (err != CborNoError)
+    return 0;
 
-    cbor_encode_uint(&array, (uint64_t) 3);
-    cbor_encode_text_stringz(&array, device_id);
-    cbor_encode_uint(&array, ts);
-    cbor_encode_boolean(&array, door_open);
-    uint8_t sig[32] = {0};
-    cbor_encode_byte_string(&array, sig, sizeof(sig));
+  cbor_encode_uint(&array, (uint64_t)3);
+  cbor_encode_text_stringz(&array, device_id);
+  cbor_encode_uint(&array, ts);
+  cbor_encode_boolean(&array, door_open);
+  uint8_t sig[32] = {0};
+  cbor_encode_byte_string(&array, sig, sizeof(sig));
 
-    err = cbor_encoder_close_container(&encoder, &array);
-    if (err != CborNoError) return 0;
+  err = cbor_encoder_close_container(&encoder, &array);
+  if (err != CborNoError)
+    return 0;
 
-    return cbor_encoder_get_buffer_size(&encoder, buf);
+  return cbor_encoder_get_buffer_size(&encoder, buf);
 }
 
 size_t encodeCurtain(uint8_t *buf, size_t buf_size,
-                  const char *device_id,
-                  uint64_t ts,
-                  uint64_t curtain)
+                     const char *device_id,
+                     uint64_t ts,
+                     uint64_t curtain)
 {
-    CborEncoder encoder, array;
-    cbor_encoder_init(&encoder, buf, buf_size, 0);
+  CborEncoder encoder, array;
+  cbor_encoder_init(&encoder, buf, buf_size, 0);
 
-    CborError err = cbor_encoder_create_array(&encoder, &array, 5);
-    if (err != CborNoError) return 0;
+  CborError err = cbor_encoder_create_array(&encoder, &array, 5);
+  if (err != CborNoError)
+    return 0;
 
-    cbor_encode_uint(&array, (uint64_t) 4);
-    cbor_encode_text_stringz(&array, device_id);
-    cbor_encode_uint(&array, ts);
-    cbor_encode_uint(&array, curtain);
-    uint8_t sig[32] = {0};
-    cbor_encode_byte_string(&array, sig, sizeof(sig));
+  cbor_encode_uint(&array, (uint64_t)4);
+  cbor_encode_text_stringz(&array, device_id);
+  cbor_encode_uint(&array, ts);
+  cbor_encode_uint(&array, curtain);
+  uint8_t sig[32] = {0};
+  cbor_encode_byte_string(&array, sig, sizeof(sig));
 
-    err = cbor_encoder_close_container(&encoder, &array);
-    if (err != CborNoError) return 0;
+  err = cbor_encoder_close_container(&encoder, &array);
+  if (err != CborNoError)
+    return 0;
 
-    return cbor_encoder_get_buffer_size(&encoder, buf);
+  return cbor_encoder_get_buffer_size(&encoder, buf);
 }
 
 void compute_hmac(const uint8_t *data, size_t data_len, uint8_t *out)
@@ -139,10 +147,11 @@ void compute_hmac(const uint8_t *data, size_t data_len, uint8_t *out)
 
 void apply_hmac_inplace(uint8_t *buf, size_t len)
 {
-  if (len < 32) return;
+  if (len < 32)
+    return;
 
   size_t sig_offset = len - 32;
-  
+
   uint8_t hmac_out[32];
   compute_hmac(buf, sig_offset, hmac_out);
 
@@ -156,7 +165,8 @@ void callback(char *topic, uint8_t *payload, size_t payload_len)
   CborParser parser;
   CborValue root, elem;
   cbor_parser_init(payload, payload_len, 0, &parser, &root);
-  if (!cbor_value_is_array(&root)) return;
+  if (!cbor_value_is_array(&root))
+    return;
 
   CborValue it;
   cbor_value_enter_container(&root, &it);
@@ -184,11 +194,11 @@ void callback(char *topic, uint8_t *payload, size_t payload_len)
 
   uint8_t validation_buf[32];
   compute_hmac(payload, payload_len - 32, validation_buf);
-  if (memcmp(hmac_buf, validation_buf, 32) != 0) {
+  if (memcmp(hmac_buf, validation_buf, 32) != 0)
+  {
     Serial.println("Invalid HMAC");
     return;
   }
-
 
   Serial.println("Received Packet:");
   Serial.print(" Type: ");
@@ -211,24 +221,27 @@ void callback(char *topic, uint8_t *payload, size_t payload_len)
   MQTT_CLIENT.publish("blinds/curtain", cbor_buffer, cbor_len);
 }
 
-void setup() {
+void setup()
+{
   // Wait for serial monitor to start
   delay(3000);
 
   // Set up serial port for tracing information
   Serial.begin(9600);
-  
+
   // Connect to Wi-Fi
   Serial.println("Connecting to Wi-Fi...");
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
 
   int attempts = 0;
-  while (WiFi.status() != WL_CONNECTED && attempts < 20) {
+  while (WiFi.status() != WL_CONNECTED && attempts < 20)
+  {
     delay(500);
     attempts++;
   }
 
-  if (WiFi.status() == WL_CONNECTED) {
+  if (WiFi.status() == WL_CONNECTED)
+  {
     Serial.println("\nWi-Fi Connected!");
     Serial.print("IP Address: ");
     IPAddress ip = WiFi.localIP();
@@ -239,9 +252,13 @@ void setup() {
     Serial.print(ip[2]);
     Serial.print(".");
     Serial.println(ip[3]);
-  } else {
+  }
+  else
+  {
     Serial.println("\nFailed to connect to Wi-Fi. Please check your credentials.");
-    while (true) {}
+    while (true)
+    {
+    }
   }
 
   // Get time
@@ -250,38 +267,48 @@ void setup() {
   // Connect to MQTT
   Serial.print("Connecting to MQTT Server...");
   MQTT_CLIENT.setServer(MQTT_SERVER_NAME, MQTT_PORT);
-  while (!MQTT_CLIENT.connected()) {
-    if (MQTT_CLIENT.connect(MQTT_CLIENT_NAME)) {
+  while (!MQTT_CLIENT.connected())
+  {
+    if (MQTT_CLIENT.connect(MQTT_CLIENT_NAME))
+    {
       Serial.println("\nMQTT connected");
       MQTT_CLIENT.setCallback(callback);
       MQTT_CLIENT.subscribe("blinds/commands");
-    } else {
+    }
+    else
+    {
       Serial.print(".");
       delay(1000);
     }
   }
 }
 
-void loop() {
+void loop()
+{
   Serial.println("Sending new packet");
   uint64_t ts = timeClient.getEpochTime();
-  
+
   uint selection = random(0, 100);
-  if (selection < 5) {
+  if (selection < 5)
+  {
     bool door_open = random(0, 2);
 
     uint8_t cbor_buffer[192];
     size_t cbor_len = encodeDoor(cbor_buffer, sizeof(cbor_buffer), MQTT_CLIENT_NAME, ts, door_open);
     apply_hmac_inplace(cbor_buffer, cbor_len);
     MQTT_CLIENT.publish("blinds/door", cbor_buffer, cbor_len);
-  } else if (selection < 25) {
+  }
+  else if (selection < 25)
+  {
     double temperature = random(50, 80);
 
     uint8_t cbor_buffer[192];
     size_t cbor_len = encodeTemperature(cbor_buffer, sizeof(cbor_buffer), MQTT_CLIENT_NAME, ts, temperature);
     apply_hmac_inplace(cbor_buffer, cbor_len);
     MQTT_CLIENT.publish("blinds/temperature", cbor_buffer, cbor_len);
-  } else {
+  }
+  else
+  {
     bool motion = random(0, 2);
 
     uint8_t cbor_buffer[192];
